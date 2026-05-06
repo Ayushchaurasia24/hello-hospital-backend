df462978fe9f3fb10c75e3d6b93386d9d7053eb7
Ayushchaurasia24 <chaurasiakd9425@gmail.com>
2026-05-06 15:29:54 +0530

    Added search based feature and toggle drop down for category

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -62,4 +62,16 @@ router.post("/", upload.array("files", 10), async (req, res) => {
   }
 });
 
+router.get("/documents", async (req, res) => {
+  try {
+    const documents = await Document.find().sort({ createdAt: -1 });
+
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
 export default router;
